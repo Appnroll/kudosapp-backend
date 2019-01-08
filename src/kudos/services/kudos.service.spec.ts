6acccddfcc932019b88df61b94781f55023c5074
dota <dota@appnroll.com>
2019-01-08 16:53:00 +0100

    Fixing services tests

--- src/kudos/services/kudos.service.spec.ts
+++ src/kudos/services/kudos.service.spec.ts
@@ -1,16 +1,35 @@
-import { Test, TestingModule } from '@nestjs/testing';
-import { KudosService } from './kudos.service';
+import {Test, TestingModule} from '@nestjs/testing';
+import {KudosService} from './kudos.service';
+import {HttpModule} from "@nestjs/common";
+import {Repository} from "typeorm";
+import {Kudos} from "../model/kudos.entity";
+import {TypeOrmConfigTestService} from "../../config/type-orm-config-test.service";
+import {TypeOrmModule} from '@nestjs/typeorm';
 
 describe('KudosService', () => {
-  let service: KudosService;
-  
-  beforeAll(async () => {
-    const module: TestingModule = await Test.createTestingModule({
-      providers: [KudosService],
-    }).compile();
-    service = module.get<KudosService>(KudosService);
-  });
-  it('should be defined', () => {
-    expect(service).toBeDefined();
-  });
+    let service: KudosService;
+    let kudosRepository: Repository<Kudos>;
+
+    beforeAll(async () => {
+        const module: TestingModule = await Test.createTestingModule({
+            imports: [
+                HttpModule,
+                TypeOrmModule.forRootAsync({
+                    useClass: TypeOrmConfigTestService,
+                })
+            ],
+            providers: [
+                KudosService,
+                {
+                    useClass: asdsa
+
+
+                }
+            ],
+        }).compile();
+        service = module.get<KudosService>(KudosService);
+    });
+    it('should be defined', () => {
+        expect(service).toBeDefined();
+    });
 });
