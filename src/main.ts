c446332278dd957b47b9cf44a9dd42ab6939fbb9
dota <dota@appnroll.com>
2019-01-08 11:30:02 +0100

    Configuration fix

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,6 @@ console.log(`${process.env.DB_HOST} - ${process.env.DB_USERNAME} - ${process.env
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
