c703a8aaf16d51f425e670ac80371687e11999f3
dotadota <michal@appnroll.com>
2019-01-09 09:42:25 +0100

    Enable cors

--- src/main.ts
+++ src/main.ts
@@ -7,6 +7,7 @@ console.log(`PORT: ${process.env.PORT} `);
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors()
   await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
