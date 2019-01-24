e070beb046b8be5010d6266ea10e26838c6af29c
dotadota <michal@appnroll.com>
2019-01-24 18:10:55 +0100

    Fix for production build

--- src/config/database.ts
+++ src/config/database.ts
@@ -1,3 +1,5 @@
+const pathToConfiguration = process.env.NODE_ENV == 'production' ? '/../**/**.entity.js' : '/../**/**.entity{.ts,.js}';
+
 export default {
     type: process.env.TYPE,
     host: process.env.DB_HOST,
@@ -5,7 +7,7 @@ export default {
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
     port: parseInt(process.env.DB_PORT),
-    entities: [ __dirname + '/../**/**.entity{.ts,.js}'],
+    entities: [ __dirname + pathToConfiguration],
     logging: process.env.LOGGING,
     synchronize: process.env.SYNCHRONIZE,
     retryAttempts: true
