5289ac5a1309aae59e6858ca7de45d81ea33501b
dota <dota@appnroll.com>
2019-02-15 13:09:02 +0100

    Pool endpoint

--- src/pool/controllers/pool.controller.ts
+++ src/pool/controllers/pool.controller.ts
@@ -7,7 +7,39 @@ export class PoolController {
   @Post()
   slackCommand(@Body() body: PostSlackDto){
     console.log(body)
-    return {text: '✅ Thanks for submitting Kudos!'}
+    return {
+      "text": "This is your first interactive message",
+      "attachments": [
+        {
+          "text": "Building buttons is easy right?",
+          "fallback": "Shame... buttons aren't supported in this land",
+          "callback_id": "button_tutorial",
+          "color": "#3AA3E3",
+          "attachment_type": "default",
+          "actions": [
+            {
+              "name": "yes",
+              "text": "yes",
+              "type": "button",
+              "value": "yes"
+            },
+            {
+              "name": "no",
+              "text": "no",
+              "type": "button",
+              "value": "no"
+            },
+            {
+              "name": "maybe",
+              "text": "maybe",
+              "type": "button",
+              "value": "maybe",
+              "style": "danger"
+            }
+          ]
+        }
+      ]
+    }
   }
 
 }
