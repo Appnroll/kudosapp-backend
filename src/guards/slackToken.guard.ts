a24858bb56d2fa898badff45f17e492fde4d5a4f
dotadota <michal@appnroll.com>
2019-02-20 12:22:21 +0100

    Little refactor Added emoji

--- src/guards/slackToken.guard.ts
+++ src/guards/slackToken.guard.ts
@@ -1,16 +1,16 @@
 import {Observable} from "rxjs";
 import {CanActivate, ExecutionContext, Injectable} from "@nestjs/common";
-import {SlackService} from "../kudos/services/slack.service";
+import {SlackHelperService} from "../services/slack-helper.service";
 
 @Injectable()
 export class SlackTokenGuard implements CanActivate {
 
-  constructor(private slackService: SlackService) {
+  constructor(private slackHelperService: SlackHelperService) {
   }
 
   canActivate(context: ExecutionContext): boolean | Promise<boolean> | Observable<boolean> {
     const request = context.switchToHttp().getRequest();
-    const timeWhenResponseUrlIsAvailable = this.slackService.getSlackResponseDelay()
+    const timeWhenResponseUrlIsAvailable = this.slackHelperService.getSlackResponseDelay()
     const validToken = process.env.SLACK_TOKEN || 'hDa8MTD79bTgTpfAQ8W6cWc4'
     let body = request.body;
 
@@ -19,7 +19,7 @@ export class SlackTokenGuard implements CanActivate {
     }
 
     if (validToken !== body.token && !body.response_url) {
-      this.slackService.responseInvalidToken(body.response_url, timeWhenResponseUrlIsAvailable)
+      this.slackHelperService.responseInvalidToken(body.response_url, timeWhenResponseUrlIsAvailable)
       return false;
     } else {
       return true;
