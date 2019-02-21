d56aaa7defb88ff854d9b41720be2acb6afcd2a2
dota <dota@appnroll.com>
2019-02-21 15:23:33 +0100

    Fixed action types

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,7 +4,7 @@ import {TypeOrmModule} from '@nestjs/typeorm';
 import {ConfigModule, ConfigService} from "nestjs-config";
 import {AvailabilityModule} from './availability/availability.module';
 import {PollModule} from './poll/poll.module';
-import { SlackHelperService } from './services/slack-helper.service';
+import {SlackHelperService} from './services/slack-helper.service';
 import * as path from 'path';
 
 const pathToConfiguration = process.env.NODE_ENV == 'production' ? 'config/**/*.js' : 'config/**/*.{ts,js}';
