d0311a07490ab5929f507a2203590fbacafd9461
dotadota <michal@appnroll.com>
2019-02-27 10:07:54 +0100

    Auth config fix

--- src/config/availability.ts
+++ src/config/availability.ts
@@ -4,5 +4,5 @@ export default {
   slackClientSecret: process.env.AVAILABILITY_SLACK_CLIENT_SECRET,
   slackRedirectUri: process.env.AVAILABILITY_SLACK_REDIRECT_URI,
   slackOAuthToken: process.env.AVAILABILITY_SLACK_AUTH_TOKEN,
-  slackAfterLoginRedirect: process.env.KUDOS_SLACK_AFTER_LOGIN_REDIRECT
+  slackAfterLoginRedirect: process.env.AVAILABILITY_SLACK_AFTER_LOGIN_REDIRECT
 };
